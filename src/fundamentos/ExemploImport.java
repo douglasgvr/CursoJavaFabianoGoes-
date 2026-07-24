f2657d4b1664fde7636e01891ac42d7f036a7348
douglasgvr <douglasfarmaangra@gmail.com>
2026-07-24 13:01:39 -0300

    feat: aprendendo sobre imports e a notacao ponto.

---
+++ src/fundamentos/ExemploImport.java
@@ -0,0 +1,12 @@
+package fundamentos;
+
+import java.util.Date;
+
+public class ExemploImport {
+    public static void main(String[] args){
+        System.out.println("Exemplo de Import");
+
+        Date data = new Date();
+        System.out.println(data);
+    }
+}
---
+++ src/fundamentos/NotacaoPOnto.java
@@ -0,0 +1,12 @@
+package fundamentos;
+
+public class NotacaoPOnto {
+    public static void main(String[] args) {
+        String texto = "Bom dia X";
+        /*Atraves do ponto . estou chamando um metodo (Subistituir o X pelo Douglas)*/
+        texto = texto.replace("X", "Douglas");
+
+
+        System.out.print(texto);
+    }
+}
